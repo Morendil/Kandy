b44ff9b6dcff74a47c66bda81e1a163e1a8066f3
Laurent Bossavit <laurent.git@bossavit.com>
2016-02-07 13:34:09 +0100

    Handle both GET and POST, do nothing unless called with a cell number

--- lib/respond.rb
+++ lib/respond.rb
@@ -2,8 +2,11 @@ require 'nexmo'
 require 'sinatra'
 require './lib/reply'
  
-get '/sms' do
-	reply_text = reply_for params
-	nexmo = Nexmo::Client.new
-	nexmo.send_message(from:'Kandy', to: params[:msisdn], text: reply_text)
+[ :get, :post ].each do |method|
+  send method, '/sms' do 
+    return unless params[:msisdn]
+    reply_text = reply_for params
+    nexmo = Nexmo::Client.new
+    nexmo.send_message(from:'Kandy', to: params[:msisdn], text: reply_text)
+  end
 end
